7be16fc2936eec895dd51d24d94be546b2b96a8c
orange <2040406891@qq.com>
2022-03-01 15:02:29 +0800

    refactor(webpack): 调整开发与编译环境

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -1,7 +1,25 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
+const path = require('path');
 
 module.exports = merge(common, {
   mode: 'development',
   cache: true,
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'public'),
+    },
+    compress: true,
+    open: true,
+    host: process.env.HOST || '127.0.0.1',
+    port: parseInt(process.env.PORT, 10) || 3000,
+    hot: true,
+    historyApiFallback: true, // 所有的404都到index.html
+    proxy: {
+      '/api': {
+        target: 'http://baidu.com',
+        changeOrigin: true,
+      },
+    }
+  }
 });
